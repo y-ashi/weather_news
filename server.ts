738a855b340d25ffc1ff508467a9e6a542c0b3c8
y-ashi <ashiyugames16@gmail.com>
2025-09-29 00:28:55 +0900

    サーバー側のimportエラー対応

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
-const express = require('express');
-const axios = require('axios');
-const dotenv = require('dotenv');
+import express from 'express';
+import dotenv from 'dotenv';
+import axios from 'axios';
+
 dotenv.config();
 
 const app = express();
